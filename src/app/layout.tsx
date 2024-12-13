6644c63251d5cb7cb7e356dc347b32765846d13c
Dmitry Karpov <karpovdu.01@gmail.com>
2024-12-13 05:14:50 +0500

    Fix hydration err and block display bug on screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,12 @@
 'use client'
 
 import { Providers } from '@/lib/providers'
-import { useEffect, useState } from 'react'
 import "./globals.css"
 
 
 
 
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>){
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <></>;
-  }
   return (
     <html lang="en">
       <body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,21 @@ import { Card, CardBody } from "@nextui-org/card"
 
 export default function Home() {
   return (
-    <main className='h-screen flex justify-center'>
-      <div className='w-[1280px]'>
-        <div className='w-full flex justify-end py-4'>
-          <ThemeSwitcher/>
-        </div>
-        <div  className='h-full flex justify-center items-center'>
-          <div className='flex flex-row gap-12'>
-            <Card className='h-40'>
-              <CardBody>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quia nesciunt facilis sit autem quos, porro hic blanditiis soluta, officiis fugit, laboriosam laborum rem! Mollitia veniam provident quis dignissimos unde.1
-              </CardBody>
-            </Card>
-            <Card>
-              <CardBody>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda similique odio nulla sit maxime officiis officia, perferendis minima deserunt laboriosam eius, natus magnam quis quaerat consequatur nihil? Cum, iste earum.
-              </CardBody>
-            </Card>
-          </div>
-        </div>
+    <main className='w-[1280px] mx-auto h-screen flex flex-col'>
+      <div className='right flex justify-end'>
+        <ThemeSwitcher/>
+      </div>
+      <div  className='h-full flex flex-row items-center justify-center gap-4'>
+          <Card>
+            <CardBody>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quia nesciunt facilis sit autem quos, porro hic blanditiis soluta, officiis fugit, laboriosam laborum rem! Mollitia veniam provident quis dignissimos unde.1
+            </CardBody>
+          </Card>
+          <Card>
+            <CardBody>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda similique odio nulla sit maxime officiis officia, perferendis minima deserunt laboriosam eius, natus magnam quis quaerat consequatur nihil? Cum, iste earum.
+            </CardBody>
+          </Card>
       </div>
     </main>
   );
